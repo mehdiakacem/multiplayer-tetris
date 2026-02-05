f162a5b6c598477e4590ce4e14fc67c8651c6717
mehdiakacem <mehdiakacem20@gmail.com>
2026-02-05 22:15:20 +0100

    migration to vitest complete

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      include: ["src/**/*.{js,jsx}"],
+      exclude: [
+        "node_modules/",
+        "./src/test/",
+        "**/*.test.{js,jsx}",
+        "**/*.config.{js,jsx}",
+        "**/index.js",
+        "src/client/main.jsx",
+      ],  
+      thresholds: {
+        statements: 70,
+        branches: 50,
+        functions: 70,
+        lines: 70,
+      },
+    },
+  },
+});
