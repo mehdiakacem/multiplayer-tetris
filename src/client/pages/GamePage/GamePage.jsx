80b91d935ee5895f3ae7423a6e7560f6021deafa
mehdiakacem <mehdiakacem20@gmail.com>
2026-02-07 17:27:36 +0100

    refactored game page

--- src/client/pages/GamePage/GamePage.jsx
+++ src/client/pages/GamePage/GamePage.jsx
@@ -1,45 +1,50 @@
 import BoardSection from "../../components/BoardSection/BoardSection.jsx";
 import Opponents from "../../components/Opponents/Opponents.jsx";
-import { useParams } from "react-router";
-import { useNavigate } from "react-router";
+import { useParams, useNavigate } from "react-router";
 import "./GamePage.css";
 import { useGameSocket } from "../../hooks/useGameSocket.js";
 import { useKeyboardInput } from "../../hooks/useKeyboardInput.js";
+import { useCallback } from "react";
 
 function GamePage() {
-  let { room, playerName } = useParams();
+  const { room, playerName } = useParams();
   const navigate = useNavigate();
 
-  const { socket, game, opponents, hostId, status } =
-    useGameSocket({
-      room,
-      playerName,
-    });
+  const { socket, game, opponents, hostId, status } = useGameSocket({
+    room,
+    playerName,
+  });
 
-  const isHost = socket.id === hostId;
+  const socketId = socket?.id;
+  const isHost = !!socketId && socketId === hostId;
 
   useKeyboardInput({
-    onInput: (action) => socket.emit("player-input", { action }),
+    onInput: (action) => socket?.emit("player-input", { action }),
     onEscape: () => navigate("/"),
   });
 
-  const handleStartClick = () => socket.emit("start-game");
+  const handleStartClick = useCallback(
+    () => socket?.emit("start-game"),
+    [socket],
+  );
 
-  const player = game?.players.find((p) => p.id === socket.id);
+  const player = game?.players?.find((p) => p.id === socketId);
+  const safeOpponents = opponents ?? [];
 
   return (
     <>
       <span>
         {playerName} {isHost && "(Host)"}
       </span>
+
       <BoardSection
         player={player}
         status={status}
         isHost={isHost}
         onRestart={handleStartClick}
       />
-      <Opponents opponents={opponents} hostId={hostId} />
-      {/* <NextPiece type={nextPieceType} /> */}
+
+      <Opponents opponents={safeOpponents} hostId={hostId} />
     </>
   );
 }
